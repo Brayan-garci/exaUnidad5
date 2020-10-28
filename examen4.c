e0a4e6010550539c8ab910b796278f1d99b102f7
repl.it user <replituser@example.com>
2020-10-28 22:41:13 +0000

    examen4

---
+++ examen4.c
@@ -0,0 +1,23 @@
+#incluide<iostream>
+
+using namespace std;
+
+int main(){
+    int n1,n2 suma = 0; resta = 0; multiplicacion = 0 ,division = 0, potencia = 0;
+    cout<<"digite un numero: "; cin>>n1;
+    cout<<"digite otro numero: "; cin>>n2;
+
+    suma = n1 + n2;
+    resta = n1 - n2;
+    multiplicacion = n1 * n2;
+    division = n1 / n2;
+    potencia = n1 ^ n2;
+
+    cout<<"\nLa suma es: "<<suma<<end1;
+    cout<<"La resta es: "<<resta<<end1;
+    cout<<"La multiplicacion es: "<<multiplicacion<<end1;
+    cout<<"la division es: "<<division<<end1;
+    cout<<"La potencia es: "<<potencia<<end1;
+
+    return 0;
+}
